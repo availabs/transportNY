183614cb2e5e34ddd52912a14cdb024d5831cdbc
alex <am3081@gmail.com>
2023-03-09 15:02:05 -0500

    changes

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,12 @@
 
 import Auth from "pages/Auth"
 import NoMatch from "pages/404"
-import DataManager from "pages/DataManagerClient"
+import DataManager from "pages/DataManager"
 import Documentation from "pages/Documentation"
 
 const Routes = [
   Auth,
-  ...DataManager,
+  ...DataManager(),
   ...Documentation,
   NoMatch
 ]
