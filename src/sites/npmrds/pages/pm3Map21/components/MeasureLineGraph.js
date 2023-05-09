ead914b2019a975053a3fbff23e3c1983992c764
Eric Conklin <eric.s.conklin@gmail.com>
2023-05-09 14:14:12 -0400

    stuff

---
+++ src/sites/npmrds/pages/pm3Map21/components/MeasureLineGraph.js
@@ -0,0 +1,63 @@
+import React from "react"
+
+import get from "lodash.get"
+import { extent as d3extent } from "d3-array"
+
+import {
+  useFalcor
+} from "modules/avl-components/src"
+
+import {
+  LineGraph
+} from "modules/avl-graph/src"
+
+const idFormat = () => "Value";
+
+const MeasureLineGraph = ({ versions, geoid, measure, name }) => {
+
+  const { falcorCache } = useFalcor();
+
+  const [data, domain] = React.useMemo(() => {
+    const values = [];
+    const data = [{
+      id: measure,
+      data: versions.reduce((a, c) => {
+        const d = get(falcorCache, ["pm3", "versionedCalculations", "version", c.version, "geolevel", geoid, "value"], null);
+        if (d) {
+          values.push(d[measure]);
+          a.push({
+            x: c.year,
+            y: d[measure]
+          })
+        }
+        return a;
+      }, [])
+    }];
+    const extent = d3extent(values);
+    return [data, [extent[0] * 0.9, extent[1]]];
+  }, [falcorCache, versions, geoid, measure]);
+
+  return (
+    <div>
+      <div className="font-bold">
+        { name }
+      </div>
+      <div>
+        <LineGraph data={ data }
+          colors={ ["black"] }
+          margin={ { left: 100, top: 5, right: 5, bottom: 25 } }
+          hoverComp={ {
+            showTotals: false,
+            yFormat: ",.2f",
+            idFormat
+          } }
+          padding={ 0.25 }
+          axisBottom={ true }
+          axisLeft={ { ticks: 5 } }
+          yScale={ { domain } }/>
+      </div>
+    </div>
+  )
+}
+
+export default MeasureLineGraph;
---
+++ src/sites/npmrds/pages/pm3Map21/components/MeasureList.js
@@ -0,0 +1,61 @@
+import React from "react"
+
+import get from "lodash.get"
+import { format as d3format } from "d3-format"
+
+import {
+  useFalcor
+} from "modules/avl-components/src"
+
+const Measure = ({ value, year, prev, color }) => {
+  return (
+    <div className="border border-current text-center">
+      <div className="border-b-2 border-current font-bold">{ year }</div>
+      <div className="border-b border-current">
+        { value }
+      </div>
+      <div style={ { color } }>
+        { prev }
+      </div>
+    </div>
+  )
+}
+
+const valueFormat = d3format(",.2f");
+const percentFormat = d3format("+,.2%")
+
+const MeasureList = ({ versions, geoid, measure, name, colors }) => {
+
+  const { falcorCache } = useFalcor();
+
+  const data = React.useMemo(() => {
+    return versions.map(({ version, year }, i) => {
+      const curr = get(falcorCache, ["pm3", "versionedCalculations", "version", version, "geolevel", geoid, "value"], null);
+      const prev = get(falcorCache, ["pm3", "versionedCalculations", "version", get(versions, [i - 1, "version"]), "geolevel", geoid, "value"], null);
+      const cv = get(curr, measure, 0);
+      const pv = get(prev, measure, null);
+      const change = pv ? (cv - pv) / pv : null;
+      return {
+        value: valueFormat(cv),
+        prev: i === 0 ? "---" : percentFormat(change),
+        color: change > 0 ? colors[2] : change < 0 ? colors[0] : colors[1],
+        year
+      };
+    })
+  }, [falcorCache, versions, geoid, measure]);
+
+  return (
+    <div>
+      <div className="font-bold">
+        { name }
+      </div>
+      <div className="grid"
+        style={ { gridTemplateColumns: `repeat(${ data.length }, minmax(0, 1fr))` } }
+      >
+        { data.map(d => <Measure key={ d.year } { ...d }/>) }
+      </div>
+    </div>
+  )
+}
+
+export default MeasureList;
