673a1cb567c403860055aaf53b38c134dbf8d584
alex <am3081@gmail.com>
2023-09-14 15:07:01 -0400

    changes

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ export const PROJECT_NAME = "NPMRDS";
 let API_HOST = "https://graph.availabs.org";
 let AUTH_HOST = "https://availauth.availabs.org";
 let CLIENT_HOST = "https://transportny.org";
-let DAMA_HOST = "https://dama-dev.availabs.org";
+let DAMA_HOST = "https://graph.availabs.org";
 
 if (process.env.NODE_ENV === "development") {
-  API_HOST = "http://localhost:4444";
+  API_HOST = "https://graph.availabs.org";
   CLIENT_HOST = "localhost:3000";
   //DAMA_HOST = "http://localhost:3369";
 }
