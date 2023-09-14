03816cd4070543b7b36d52f5fb3af75bd04a4ec0
Eric Conklin <eric.s.conklin@gmail.com>
2023-09-14 14:54:22 -0400

    stuff

--- src/sites/freightdata/index.js
+++ src/sites/freightdata/index.js
@@ -1,35 +1,20 @@
 import DataManager from "~/pages/DataManager"
 
-<<<<<<< HEAD
 import MapPageConfig from "./MapPage"
 
-=======
->>>>>>> a50e5aa66e94187c557f563da708d719cf3bc19b
 import { useFalcor } from "~/modules/avl-components/src"
 import { useAuth } from "@availabs/ams"
 
 const DAMA_ARGS = {
-<<<<<<< HEAD
-  baseUrl: "",
-  defaultPgEnv: "freight_data",
-  auth: true,
-=======
   baseUrl: '',
   defaultPgEnv: 'freight_data',
->>>>>>> a50e5aa66e94187c557f563da708d719cf3bc19b
   useFalcor,
   useAuth
 }
 
-<<<<<<< HEAD
 const Routes = [
   ...DataManager(DAMA_ARGS),
   MapPageConfig
-=======
-
-const Routes = [
-  ...DataManager(DAMA_ARGS)
->>>>>>> a50e5aa66e94187c557f563da708d719cf3bc19b
 ]
 
 const site = {
