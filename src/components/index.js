f2c9816009b247e69fe1e81086cc7a70c7a03025
alex <am3081@gmail.com>
2024-04-09 14:25:36 -0400

    test 123

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import DamaMap from './DamaMap.jsx'
+
+
+const ComponentRegistry = {
+	"Map: Dama Map": DamaMap,
+}
+
+export default ComponentRegistry
--- src/sites/freightatlas/pages/freightAtlasCMS.jsx
+++ src/sites/freightatlas/pages/freightAtlasCMS.jsx
@@ -8,13 +8,28 @@ import {Logo} from '~/layout/ppdaf-layout'
 import AuthMenu from "~/pages/Auth/AuthMenu"
 
 import siteConfig from '~/modules/dms/src/patterns/page/siteConfig'
-//import ComponentRegistry from '~/component_registry'
+import ComponentRegistry from '~/components'
 import Selector, { registerComponents } from "~/modules/dms/src/patterns/page/selector"
+import { DamaMap } from '~/pages/DataManager'
+
 // import BuildingFootprintsDownload from "./buildings_download"
 
+registerComponents({
+  "Map: Dama Map": DamaMap,
+})
 //registerComponents(ComponentRegistry)
 registerDataType("selector", Selector)
 
+console.log('components', DamaMap)
+
+
+const theme = {
+  page: {
+    wrapper1: 'w-full flex-1 flex flex-col  ', // first div inside Layout
+    wrapper2: 'w-full h-full flex-1 flex flex-row', // inside page header, wraps sidebar
+    wrapper3: 'flex flex-1 w-full flex-col border shadow bg-white relative text-md font-light leading-7 min-h-[calc(100vh_-_51px)]', // content wrapepr
+  }
+}
 
 const Routes = [
   {
@@ -26,10 +41,13 @@ const Routes = [
         logo: <Logo />, 
         rightMenu: <AuthMenu />,
         baseUrl: "",
-        checkAuth
+        checkAuth,
+        theme,
+        pgEnv:'freight_data'
       }), 
       "/",
-      withAuth
+      withAuth,
+      
     ),
     authLevel: 5,
     name: "CMS",
