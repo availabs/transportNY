56a45e504418e37d6be3e7b37ff9e66dfd3a5e3c
Alex Muro <am3081@gmail.com>
2024-08-01 15:09:06 -0400

    merge conflicts

--- src/config.js
+++ src/config.js
@@ -6,17 +6,10 @@ let CLIENT_HOST = "https://transportny.org";
 let DAMA_HOST = "https://graph.availabs.org";
 
 if (process.env.NODE_ENV === "development") {
-<<<<<<< HEAD
   //API_HOST = "http://localhost:4444";
   //DAMA_HOST = "http://localhost:4444";
   //CLIENT_HOST = "localhost:5173";
   // AUTH_HOST = "http://localhost:4444";
-=======
-  API_HOST = "http://localhost:4444";
-  DAMA_HOST = "http://localhost:4444";
-  CLIENT_HOST = "localhost:5173";
-  AUTH_HOST = "http://localhost:4444";
->>>>>>> 762a0a62e623be7fcc3bd58c698227ea880df081
 }
 
 export { API_HOST, AUTH_HOST, CLIENT_HOST, DAMA_HOST };
--- src/sites/freightatlas/pages/freightAtlasCMS.jsx
+++ src/sites/freightatlas/pages/freightAtlasCMS.jsx
@@ -2,23 +2,17 @@ import { useFalcor } from "~/modules/avl-components/src"
 import { useAuth } from "~/modules/ams/src";
 import {Link} from 'react-router-dom'
 
-<<<<<<< HEAD
-import { dmsPageFactory, registerDataType, registerComponents, Selector } from "~/modules/dms/src"
-import { withAuth } from "~/modules/ams/src" 
-=======
+
 import { dmsPageFactory, registerDataType, Selector, registerComponents } from "~/modules/dms/src"
 import { withAuth } from "~/modules/ams/src"
->>>>>>> 762a0a62e623be7fcc3bd58c698227ea880df081
+
 import checkAuth  from "~/layout/checkAuth"
 import {Logo} from '~/layout/ppdaf-layout'
 import AuthMenu from "~/pages/Auth/AuthMenu"
 
 import {siteConfig} from '~/modules/dms/src/patterns/page/siteConfig'
 import ComponentRegistry from '~/components'
-<<<<<<< HEAD
-=======
-// import Selector, { registerComponents } from "~/modules/dms/src/patterns/page/selector"
->>>>>>> 762a0a62e623be7fcc3bd58c698227ea880df081
+
 import { DamaMap } from '~/pages/DataManager'
 
 // import BuildingFootprintsDownload from "./buildings_download"
