c1129a4474356d515e6f7c7bbcb974ba175b7e1e
sarangp3010 <spatel25@albany.edu>
2024-08-07 11:51:24 -0400

    add view page

---
+++ src/pages/TransportNYDataTypes/npmrds_raw/overview.jsx
@@ -0,0 +1,112 @@
+import React, { useMemo } from "react";
+import { groupBy, orderBy } from "lodash";
+
+export default function NpmrdsRawOverview({
+  views,
+}) {
+  const groupbyState = useMemo(() => {
+    return groupBy(
+      orderBy(
+        views.filter(v => v && v.metadata),
+        ["metadata.start_date", "metadata.end_date"],
+        ["asc", "asc"]
+      ),
+      (v) => v?.metadata?.state_code
+    );
+  }, [views]);
+
+  const headers = [
+    "State",
+    "View Id",
+    "Version",
+    "Start Date",
+    "End Date",
+    "Tmcs",
+  ];
+
+  return (
+    <div className="w-full p-5">
+      <div className="flex m-3">
+        <div className="justify-start w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label className="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2">
+            View Data
+          </label>
+        </div>
+      </div>
+
+      {Object.keys(groupbyState).length ? (
+        <div className="overflow-x-auto px-5 py-3">
+          <table className="min-w-full bg-white">
+            <thead>
+              <tr>
+                {headers.map((key) => (
+                  <th
+                    key={key}
+                    className="py-2 px-4 bg-gray-200 text-left border-b"
+                  >
+                    {key}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(groupbyState).map((group) => (
+                <React.Fragment key={group}>
+                  {groupbyState[group].map((item, index) => (
+                    <tr key={index}>
+                      {index === 0 && (
+                        <td
+                          rowSpan={groupbyState[group].length}
+                          className="py-2 px-4 border-b font-bold"
+                        >
+                          {group}
+                        </td>
+                      )}
+                      <td
+                        key={`${group}.${item?.view_id}`}
+                        className="py-2 px-4 border-b"
+                      >
+                        {item?.view_id}
+                      </td>
+                      <td
+                        key={`${group}.${item?.metadata?.npmrds_version}`}
+                        className="py-2 px-4 border-b"
+                      >
+                        {item?.metadata?.npmrds_version}
+                      </td>
+                      <td
+                        key={`${group}.${item?.metadata?.start_date}`}
+                        className="py-2 px-4 border-b"
+                      >
+                        {item?.metadata?.start_date}
+                      </td>
+                      <td
+                        key={`${group}.${item?.metadata?.end_date}`}
+                        className="py-2 px-4 border-b"
+                      >
+                        {item?.metadata?.end_date}
+                      </td>
+                      <td
+                        key={`${group}.${item?.metadata?.no_of_tmc}`}
+                        className="py-2 px-4 border-b"
+                      >
+                        {item?.metadata?.no_of_tmc}
+                      </td>
+                    </tr>
+                  ))}
+                </React.Fragment>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div
+          className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-red-400"
+          role="alert"
+        >
+          <span className="font-medium">No Views available</span>
+        </div>
+      )}
+    </div>
+  );
+}
