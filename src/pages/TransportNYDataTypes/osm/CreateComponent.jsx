31b81c77869708dda60010efb9baf46b1603be4c
Eric Conklin <eric.s.conklin@gmail.com>
2025-05-02 15:16:46 -0400

    stuff

--- src/pages/TransportNYDataTypes/osm/CreateComponent.jsx
+++ src/pages/TransportNYDataTypes/osm/CreateComponent.jsx
@@ -40,7 +40,7 @@ const CreateComponent = ({ source }) => {
 
   return (
     <div className="grid grid-cols-2 gap-4 mt-4">
-      <input type="file"
+      <input type="file
         className="bg-white px-4 py-2 rounded"
         onChange={ handleOnChange }/>
       <button onClick={ doUploadOSMFILE }
