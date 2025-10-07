2bc6e68f2441b154a6238e86f5feab5901108eff
Alex Muro <am3081@gmail.com>
2025-10-07 14:40:36 -0400

    no ams

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from './App';
 import { API_HOST, AUTH_HOST, PROJECT_NAME, CLIENT_HOST } from './config'
 
 import { Provider } from 'react-redux';
-import store from './store';
+
 
 import PPDAF_THEME from "./theme"
 import {
@@ -22,26 +22,24 @@ import reportWebVitals from './reportWebVitals';
 //   enableAuth
 // } from "@availabs/ams"
 
-import {
-  enableAuth
-} from "~/modules/ams/src"
+// import {
+//   enableAuth
+// } from "~/modules/ams/src"
 
 import "./index.css";
 
 export const falcor = falcorGraph(API_HOST)
 
-const AuthEnabledApp = enableAuth(App, { AUTH_HOST, PROJECT_NAME, CLIENT_HOST });
+//const AuthEnabledApp = enableAuth(App, { AUTH_HOST, PROJECT_NAME, CLIENT_HOST });
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   	<Provider store={ store }>
   		<FalcorProvider falcor={ falcor }>
         <ThemeContext.Provider value={PPDAF_THEME}>
-          <AuthEnabledApp />
+          <App />
         </ThemeContext.Provider>
       </FalcorProvider>
-  	</Provider>
   </React.StrictMode>
 );
 
