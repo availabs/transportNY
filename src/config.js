efe6d8d938d009b24f7672644afaba42ac3df9b7
Alex Muro <am3081@gmail.com>
2026-03-25 11:29:22 -0400

    merge bug fix

--- src/config.js
+++ src/config.js
@@ -6,10 +6,10 @@ let CLIENT_HOST = "https://transportny.org";
 let DAMA_HOST = "https://graph.availabs.org";
 
 if (process.env.NODE_ENV === "development") {
-  API_HOST = "http://localhost:4444";
-  DAMA_HOST = "http://localhost:4444";
-  CLIENT_HOST = "localhost:5173";
-  AUTH_HOST = "http://localhost:4444";
+  // API_HOST = "http://localhost:4444";
+  // DAMA_HOST = "http://localhost:4444";
+  // CLIENT_HOST = "localhost:5173";
+  // AUTH_HOST = "http://localhost:4444";
 }
 
 export { API_HOST, AUTH_HOST, CLIENT_HOST, DAMA_HOST };
